ddc93a68813251efa15771141c57c69da4186e51
LiuYangJun <1172275496@qq.com>
2019-06-12 17:13:41 +0800

    complete the template  data-bind.

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,5 +1,5 @@
 // pages/movies/movies.js
-var app=getApp();
+var app = getApp();
 Page({
 
   /**
@@ -12,73 +12,96 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    var inTheatersUrl=app.globalData.doubanBase+"/v2/movie/in_theaters";
-    var comingSoonUrl = app.globalData.doubanBase +"/v2/movie/coming_soon";
-    var top250Url = app.globalData.doubanBase +"/v2/movie/top250";
+  onLoad: function(options) {
+    var inTheatersUrl = app.globalData.doubanBase + "/v2/movie/in_theaters" + "?start=0&count=3";
+    var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3";
+    var top250Url = app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3";
     this.getMovieListData(inTheatersUrl);
-    this.getMovieListData(comingSoonUrl);
-    this.getMovieListData(top250Url);
+    // this.getMovieListData(comingSoonUrl);
+    // this.getMovieListData(top250Url); 
   },
-  getMovieListData:function(url){
+  getMovieListData: function(url) {
+    var that = this;
     wx.request({
       url: url,
-      method:'GET',
-      header:{
-        "content-type":""
+      method: 'GET',
+      header: {
+        "content-type": ""
       },
-      success:function(res){
-        console.log(res);
+      success: function(res) {
+        that.processDoubanData(res.data);
       }
     })
+  },
+  processDoubanData: function(moviesDouban) {
+    var movies = [];
+    for (var idx in moviesDouban.subjects) {
+      var subject = moviesDouban.subjects[idx];
+      var title = subject.title;
+      if (title > 6) {
+        title = title.substring(0, 6) + '...';
+      }
+      var temp = {
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id,
+      }
+      movies.push(temp);
+
+    }
+
+    this.setData({
+      movies:movies,
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
