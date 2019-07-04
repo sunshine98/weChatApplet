817a88799592a43c5e63db83f75783bd52bb3605
YangJunLiu <1172275496@qq.com>
2019-07-04 15:08:45 +0800

    修改WIFI界面配色

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -28,7 +28,7 @@ Page({
   onMovieTap: function(event) {
     var movieId = event.currentTarget.dataset.movieid;
     wx.navigateTo({
-      url: 'movie-detail/movie-detail?id='+movieId,
+      url: 'movie-detail/movie-detail?id=' + movieId,
     })
   },
   /**
